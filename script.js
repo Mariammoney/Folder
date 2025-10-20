9b85dc20763ad81610399b58743d8a5a92f156f5
Mariam <gbadamosimariam49@gmail.com>
2025-10-20 12:59:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const toggleBtn = document.getElementById("menu");
+const sidebar = document.getElementById("sidebar");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+
+
+
+
+
+
+
+  
+});
